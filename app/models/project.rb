4409ab883ab034ad23970f3e9c059fcebcaaa416
Pavel Palancica <pavel.palancica@gmail.com>
2018-08-20 17:39:30 +0300

    Added raty jquery plugin to display star images for reviews

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,7 @@ class Project < ApplicationRecord
 
   has_many :subscriptions
   has_many :users, through: :subscriptions
+  has_many :reviews
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 500 }
@@ -16,5 +17,9 @@ class Project < ApplicationRecord
 
   def shortname
     name.length > 25 ? name[0..25] + "..." : name
+  end
+
+  def average_rating
+    reviews.blank? ? 0 : reviews.average(:star).round(2)
   end
 end
